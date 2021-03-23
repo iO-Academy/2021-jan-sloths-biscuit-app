f28e40d23dd158b86fbba4f4749360d129680262
JoJo <62075451+CaffeinatedCoder91@users.noreply.github.com>
2021-03-23 13:16:56 +0000

    Made the html skeleton and add the header on the css

--- src/index.php
+++ src/index.php
@@ -1,3 +1,33 @@
 <?php
 
 namespace BiscuitApp;
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>IBD</title>
+    <link rel="stylesheet" href="script.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Pangolin&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<main>
+    <div class="header-container">
+        <h1>That's the Way the Cookie Crumbles</h1>
+    </div>
+    <div  class="outer-container">
+        <div class="card">
+            <div class="item-header"></div>
+            <div class="item-img"></div>
+            <div class="item-data"></div>
+        </div>
+    </div>
+</main>
+
+</body>
+</html>
+
