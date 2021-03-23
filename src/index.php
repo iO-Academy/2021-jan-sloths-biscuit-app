303719b85cef3b94878d118fd81cacbe3f65a156
Waseem Brare <waseembrare@gmail.com>
2021-03-23 14:48:07 +0000

    changed outer-container class to have a media query for screen sizes less than 700 px to have a margin of 50px. script.css renamed to styles.css. alt tags added into html as dummy for php

--- src/index.php
+++ src/index.php
@@ -9,7 +9,7 @@ namespace BiscuitApp;
 <head>
     <meta charset="UTF-8">
     <title>IBD</title>
-    <link rel="stylesheet" href="script.css">
+    <link rel="stylesheet" href="styles.css">
     <link rel="preconnect" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css2?family=Pangolin&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
 </head>
@@ -20,6 +20,41 @@ namespace BiscuitApp;
         <h1>That's the Way the Cookie Crumbles</h1>
     </div>
     <div  class="outer-container">
+        <div class="card">
+            <div class="item-header"><h2>Digestive</h2></div>
+            <div class="item-img"><img src="https://placekitten.com/200/250" alt=""></div>
+            <div class="item-data"><h3>RDT: 5</h3></div>
+        </div>
+        <div class="card">
+            <div class="item-header"><h2>Digestive</h2></div>
+            <div class="item-img"><img src="https://placekitten.com/200/250" alt=""></div>
+            <div class="item-data"><h3>RDT: 5</h3></div>
+        </div>
+        <div class="card">
+            <div class="item-header"><h2>Digestive</h2></div>
+            <div class="item-img"><img src="https://placekitten.com/200/250" alt=""></div>
+            <div class="item-data"><h3>RDT: 5</h3></div>
+        </div>
+        <div class="card">
+            <div class="item-header"><h2>Digestive</h2></div>
+            <div class="item-img"><img src="https://placekitten.com/200/250"></div>
+            <div class="item-data"><h3>RDT: 5</h3></div>
+        </div>
+        <div class="card">
+            <div class="item-header"><h2>Digestive</h2></div>
+            <div class="item-img"><img src="https://placekitten.com/200/250"></div>
+            <div class="item-data"><h3>RDT: 5</h3></div>
+        </div>
+        <div class="card">
+            <div class="item-header"><h2>Digestive</h2></div>
+            <div class="item-img"><img src="https://placekitten.com/200/250"></div>
+            <div class="item-data"><h3>RDT: 5</h3></div>
+        </div>
+        <div class="card">
+            <div class="item-header"><h2>Digestive</h2></div>
+            <div class="item-img"><img src="https://placekitten.com/200/250"></div>
+            <div class="item-data"><h3>RDT: 5</h3></div>
+        </div>
         <div class="card">
             <div class="item-header"><h2>Digestive</h2></div>
             <div class="item-img"><img src="https://placekitten.com/200/250"></div>
