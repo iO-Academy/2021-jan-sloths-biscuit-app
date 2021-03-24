d9bcca7934c473e35d19c498668dd06a34f6f74b
Karen Schulz <sirk.schulz@gmail.com>
2021-03-24 13:38:32 +0000

    mixed of both tests.

--- src/test/BiscuitDisplayerTest.php
+++ src/test/BiscuitDisplayerTest.php
@@ -16,12 +16,11 @@ class BiscuitDisplayerTest extends TestCase
         $biscuitMock->expects($this->once())->method('getName')->willReturn('Bourbon');
         $biscuitMock->expects($this->once())->method('getImg')->willReturn('assets/images/bourbons-unedited.jpg');
         $biscuitMock->expects($this->once())->method('getRDT')->willReturn('7');
-        $biscuitMockArray = [$biscuitMock];
+        $input = [$biscuitMock];
 
-        $biscuitDisplayerTest = new BiscuitDisplayer();
 
         $expectedOutput = '<div class="card"><div class="item-header"><h2>Bourbon</h2></div><div class="item-img"><img src="assets/images/bourbons-unedited.jpg" alt="Bourbon biscuit"></div><div class="item-data"><h3>RDT: 7</h3></div></div>';
-        $actualOutput = $biscuitDisplayerTest->printBiscuits($biscuitMockArray);
+        $actualOutput = BiscuitDisplayer::printBiscuits($input);
         $this->assertEquals($expectedOutput, $actualOutput);
     }
 }
