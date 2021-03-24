44f923e206be7162c4bf879213358434157f797c
Louise de Mattos <72737315+Emily774@users.noreply.github.com>
2021-03-24 11:05:20 +0000

    Removed the foreach loop from the test function, created a definitioin for the object-array parameter to pass in and called the Biscuit display function in preparation for unit tests

--- src/test/BiscuitDisplayerTest.php
+++ src/test/BiscuitDisplayerTest.php
@@ -1,32 +1,46 @@
 <?php
 
 namespace BiscuitApp\Test;
+use BiscuitApp\BiscuitDisplayer;
+
+require_once "../BiscuitDisplayer.php";
+
+use BiscuitApp\BiscuitDisplayer;
 
 class BiscuitDisplayerTest extends TestCase
 {
-    public function testSuccessBiscuitDisplayer() {
-        $allBiscuitsObject = [{ ["name"]=> "Bourbon", ["img"]=> "assets/images/bourbons-unedited.jpg", ["RDT"]=> "7", ["desc"]=> ["wikipedia"]=> "https://en.wikipedia.org/wiki/Bourbon_biscuit", ["ID"]=> "1", ["description"]=> "The Bourbon biscuit is a sandwich style biscuit consisting of two thin rectangular dark chocolate-flavoured biscuits with a chocolate buttercream filling." }];
-        $biscuitString = '';
-        forEach($allBiscuitsObject as $biscuitCard) {
+    public function testSuccessprintBiscuits()
+    {
+        $jammyDodger = new BiscuitDisplayer();
 
-            $biscuitString .=
-                '<div class="card"><div class="item-header"><h2>' .
-                $biscuitCard->name .
-                '</h2></div><div class="item-img"><img src="' . $biscuitCard->img . '" alt="' . $biscuitCard->name . ' biscuit' .
-                '"></div>
-                <div class="item-data"><h3>RDT: ' . $biscuitCard->RDT .
-                '</h3></div></div>';
-        }
-        return $biscuitString;
+         $allBiscuitsObject = [
+            "name" => "Bourbon",
+            "img" => "assets/images/bourbons-unedited.jpg",
+            "RDT" => "7"
+        ];
 
+        $jammyDodger::printBiscuits($allBiscuitsObject);
 
         $expectedOutput = '<div class="card"><div class="item-header"><h2>Bourbon</h2></div><div class="item-img"><img src="assets/images/bourbons-unedited.jpg" alt="Bourbon biscuit"></div>
                 <div class="item-data"><h3>RDT: 7</h3></div></div>';
-        $actualOutput = printBicuits('');
+        $actualOutput = $jammyDodger::printBiscuits($allBiscuitsObject);
 
         $this->assertEquals($expectedOutput, $actualOutput);
     }
 }
-}
+
+
+
+
+//forEach($allBiscuitsObject as $biscuitCard) {
+//    $biscuitString .=
+//        '<div class="card"><div class="item-header"><h2>' .
+//        $biscuitCard->name .
+//        '</h2></div><div class="item-img"><img src="' . $biscuitCard->img . '" alt="' . $biscuitCard->name . ' biscuit' .
+//        '"></div>
+//                <div class="item-data"><h3>RDT: ' . $biscuitCard->RDT .
+//        '</h3></div></div>';
+//}
+
 
 
