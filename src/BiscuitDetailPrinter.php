0301efb482c548d3332ae4a9e8f42818f236b602
Mike Martin <mtmgoliath@gmail.com>
2021-03-24 16:55:02 +0000

    hopefully better

--- src/BiscuitDetailPrinter.php
+++ src/BiscuitDetailPrinter.php
@@ -20,10 +20,10 @@ class BiscuitDetailPrinter
 
         $biscuitString =
             '<img class="bg-image" src="' . $image .'"><div class="header-container"><h1>That\'s the Way the Cookie Crumbles</h1></div><div  class="outer-container">
-           <div class="card"><div class="card"><div class="title-container"><div class="item-header">
+           <div class="card"><div class="title-container"><div class="item-header">
 <h2>' . $name . '</h2></div></div>
 <div class="column-container"><div class="item-img"><img src="' . $image . '" alt="' . $name . ' biscuit' . '"></div></div>
-<div class="item-description-container"><div class="item-description"><p>' . $description . '</p></div><div class="item-data"><h3>RDT: ' . $RDT . '</h3></div><div class="wiki-btn"><a href="' . $wikipedia . '"><p>Wikipedia page</p></a><div class="back-btn"><a href="index.php"><img src="assets/images/bourbon-btn.png"></a></div></div></div>';
+<div class="item-description-container"><div class="item-description"><p>' . $description . '</p></div><div class="item-data"><h3>RDT: ' . $RDT . '</h3></div><div class="wiki-btn"><a href="' . $wikipedia . '"><p>Wikipedia page</p></a></div></div></div><div class="back-btn icon-animation"><a href="index.php"><img src="assets/images/bourbon-btn.png"></a></div></div></div>';
         return $biscuitString;
     }
 }
