c49d03cb2f3840a63ddc5c34e8a4e0b7890d8180
Waseem Brare <waseembrare@gmail.com>
2021-03-24 16:47:30 +0000

    changed styles.css

--- src/singleBiscuit.php
+++ src/singleBiscuit.php
@@ -1,19 +1,19 @@
 <?php
 
-namespace BiscuitApp;
-
-require_once 'vendor/autoload.php';
-
-use PDO;
-
-$id = $_GET['id'];
-
-// connecting to biscuit database
-$db = new PDO('mysql:host=db; dbname=biscuits', 'root', 'password');
-$query = $db->prepare('SELECT * FROM `biscuits` WHERE `id` = ?');
-$query->setFetchMode(PDO::FETCH_CLASS, Biscuit::class);
-$query->execute([$id]);
-$singleBiscuitObject = $query->fetch()
+//namespace BiscuitApp;
+//
+//require_once 'vendor/autoload.php';
+//
+//use PDO;
+//
+//$id = $_GET['id'];
+//
+//// connecting to biscuit database
+//$db = new PDO('mysql:host=db; dbname=biscuits', 'root', 'password');
+//$query = $db->prepare('SELECT * FROM `biscuits` WHERE `id` = ?');
+//$query->setFetchMode(PDO::FETCH_CLASS, Biscuit::class);
+//$query->execute([$id]);
+//$singleBiscuitObject = $query->fetch()
 
 
 
@@ -32,11 +32,29 @@ $singleBiscuitObject = $query->fetch()
 <body>
 
 <main>
+<!--    <div class="bg-image"></div>-->
+    <img class="bg-image" src="assets/images/gingernut.jpg">
     <div class="header-container">
         <h1>That's the Way the Cookie Crumbles</h1>
     </div>
     <div  class="outer-container">
-        <?php echo BiscuitDetailPrinter::printBiscuit($singleBiscuitObject) ?>
+<!--        --><?php //echo BiscuitDetailPrinter::printBiscuit($singleBiscuitObject) ?>
+        <div class="card">
+            <div class="title-container">
+                <div class="item-header"><h2>Gingernut</h2></div>
+            </div>
+            <div class="column-container">
+                <div class="item-img"><img src="assets/images/gingernut.jpg" alt=""></div>
+                <div class="item-description-container">
+                    <div class="item-description"><p>A Ginger Nut is a globally popular biscuit flavoured with ginger. Ginger snaps are flavoured with powdered ginger and a variety of other spices, most commonly cinnamon, molasses and clove.</p></div>
+                    <div class="item-data"><h3>RDT: 5</h3></div>
+                    <div class="wiki-btn"><a href="https://en.wikipedia.org/wiki/Ginger_snap"><p class="wiki-btn-text icon-animation">Wikipedia page</p></a></div>
+                </div>
+            </div>
+            <div class="back-btn icon-animation">
+                <a href="index.php"><img src="assets/images/bourbon-btn.png"></a>
+            </div>
+        </div>
     </div>
 </main>
 
