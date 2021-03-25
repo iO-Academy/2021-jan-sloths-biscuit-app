c919b595bf838bf8ca1b22bc7b1ffea320f705d6
Mike Martin <mtmgoliath@gmail.com>
2021-03-25 12:20:04 +0000

    unit test for story 3 good, s2 expected needs updating

--- src/test/BiscuitDisplayerTest.php
+++ src/test/BiscuitDisplayerTest.php
@@ -16,17 +16,22 @@ class BiscuitDisplayerTest extends TestCase
         $biscuitMock->expects($this->once())->method('getName')->willReturn('Bourbon');
         $biscuitMock->expects($this->once())->method('getImg')->willReturn('assets/images/bourbons-unedited.jpg');
         $biscuitMock->expects($this->once())->method('getRDT')->willReturn('7');
+        $biscuitMock->expects($this->once())->method('getID')->willReturn('1');
+        $biscuitMock->expects($this->once())->method('getDescription')->willReturn('testdesc');
+        $biscuitMock->expects($this->once())->method('getWikipedia')->willReturn('testurl');
 
         $biscuitMockTwo = $this->createMock(Biscuit::class);
         $biscuitMockTwo->expects($this->once())->method('getName')->willReturn('Danny');
         $biscuitMockTwo->expects($this->once())->method('getImg')->willReturn('assets/images/bourbons-unedited.jpg');
-        $biscuitMockTwo->expects($this->once())->method('getRDT')->willReturn('111');
-
+        $biscuitMockTwo->expects($this->once())->method('getRDT')->willReturn('7');
+        $biscuitMockTwo->expects($this->once())->method('getID')->willReturn('1');
+        $biscuitMockTwo->expects($this->once())->method('getDescription')->willReturn('testdesc');
+        $biscuitMockTwo->expects($this->once())->method('getWikipedia')->willReturn('testurl');
 
         $input = [$biscuitMock, $biscuitMockTwo];
 
 
-        $expectedOutput = '<div class="card"><div class="item-header"><h2>Bourbon</h2></div><div class="item-img"><img src="assets/images/bourbons-unedited.jpg" alt="Bourbon biscuit"></div><div class="item-data"><h3>Recommended dunking time:</h3><p class="number-display">7 seconds</p></div></div>'.'<div class="card"><div class="item-header"><h2>Danny</h2></div><div class="item-img"><img src="assets/images/bourbons-unedited.jpg" alt="Danny biscuit"></div><div class="item-data"><h3>Recommended dunking time:</h3><p class="number-display">111 seconds</p></div></div>';
+        $expectedOutput = '<div class="card"><a href="singleBiscuit.php?id=1"><div class="item-header"><h2>Bourbon</h2></div><div class="item-img"><img src="assets/images/bourbons-unedited.jpg" alt="Bourbon biscuit"></div><div class="item-data"><h3>Recommended dunking time:</h3><p class="number-display">7 seconds</p></div></div>' . '<div class="card"><a href="singleBiscuit.php?id=1"><div class="item-header"><h2>Danny</h2></div><div class="item-img"><img src="assets/images/bourbons-unedited.jpg" alt="Bourbon biscuit"></div><div class="item-data"><h3>Recommended dunking time:</h3><p class="number-display">7 seconds</p></div></div>'
         $actualOutput = BiscuitDisplayer::printBiscuits($input);
         $this->assertEquals($expectedOutput, $actualOutput);
     }
